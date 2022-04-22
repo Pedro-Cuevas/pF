30fa65aa65c01c4b4cdc49577da91a4131cac01b
Isabela Goetsch <isabela.getsch@gmail.com>
2022-04-22 18:43:16 +0200

    se muestran todas las ofertas

---
+++ src/main/resources/static/assets/js/search.js
@@ -0,0 +1,32 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// gets the new list of offers and displays it
+const getOffersAndDisplay = async () => {
+    let request = await fetch("/api/v1/offers", {
+        method: 'GET',
+    });
+
+    if(request.ok) {
+        let res = await request.json();
+        console.log(res);
+
+        let text = '<center> <ul class="list-group">';
+        res.forEach(obj => {
+            text += '<li class="list-group-item">'
+            +  obj.offerName + ', de ' + obj.dateBegining + ' a ' + obj.dateEnd
+            + '<div class="btn-group" role="group" aria-label="button group" style="float:right"> <button type="submit" class="btn btn-primary"'
+            + ' id="' + obj.id
+            + 'select">Seleccionar</button></div> </li>';
+        });
+        text += '</ul> </center>';
+        $('#offerList').html(text);
+
+        res.forEach(obj => {
+            $('#' + obj.id + 'select').click(() => console.log("click"));
+        });
+    }
+}
+
+getOffersAndDisplay();
+
+
+
