0b948bfbdb2d09e529eb20dadda7cbc540077a15
Isabela Goetsch <isabela.getsch@gmail.com>
2022-04-23 10:34:30 +0200

    trabajo con index

--- src/main/resources/static/assets/js/index.js
+++ src/main/resources/static/assets/js/index.js
@@ -1,11 +1,45 @@
-const getNombre = () => {
+/*const getNombre = () => {
     return localStorage.getItem("userName");
 }
 
 const setNombre = (nombre) => {
     document.getElementById("nombreLogin").innerHTML = nombre;
 }
+*/
 
-setNombre(getNombre());
+//if(document.getElementById("nombreLogin").innerHTML == "login / registro"){
+//    Storage.clear();
+//}
 
-//no funciona todavía
+//localStorage.array.forEach(element => {
+//    localStorage.removeItem(element);
+//});
+/*
+const clearStorage = () => {
+    let keys = [];
+   
+    // get storage keys
+    $.each(localStorage, (key) => {
+      keys.push(key);
+    });
+   
+    // loop through keys
+    for (let i = 0; i < keys.length; i++) {
+      let key = keys[i];
+           localStorage.removeItem(key);
+    }
+};
+
+localStorage.setItem("hayLogin", false);
+const setNombreUsuario = () => {
+    if(localStorage.getItem("hayLogin") == true){
+        setNombre(getNombre());
+    } else {
+        setNombre("login/registro");
+        console.log("hola");
+    }
+}*/
+
+//setNombreUsuario();
+
+//setNombre("hola");
