6c0cf0b0dc2c07276f8e08406329e191bb0a7ee1
Isabela Goetsch <isabela.getsch@gmail.com>
2022-05-14 11:47:23 +0200

    navbar

--- src/main/resources/static/assets/js/sugerencias.js
+++ src/main/resources/static/assets/js/sugerencias.js
@@ -24,3 +24,35 @@ const createSugerencia = async () => {
 $('#btnSend').click(() => createSugerencia());
 $('#btnClose1').click(() => $('#modal').modal('hide'));
 $('#btnClose2').click(() => $('#modal').modal('hide'));
+
+///////////////////////////////////////////////////////////
+const setNombre = (nombre) => {
+    document.getElementById("nombreLogin").innerHTML = nombre;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
+const getLogin = async () => {
+    let request = await fetch("/api/v1/login/logged", {
+        method: 'GET',
+    });
+
+    localStorage.setItem("hayLogin", false);
+    if(request.ok) {
+        let obj = await request.json();
+        //esto da error cuando no hay ningún usuario logged in
+        if(obj != null){
+            localStorage.setItem("hayLogin", true);
+            let request2 = await fetch("/api/v1/users/"+obj.userId, {
+                method: 'GET',
+            });
+            if(request2.ok){
+                let user = await request2.json();
+                localStorage.setItem("userLoggedIn", JSON.stringify(user));
+                setNombre(user.userName);
+            }
+        }
+    }
+}
+
+
+getLogin();
