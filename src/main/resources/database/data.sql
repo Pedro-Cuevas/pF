8fc45b7cfb641f6db0d945879114dcf12442b3c6
Pedro-Cuevas <prcuevasolarte@gmail.com>
2022-05-14 23:35:43 +0200

    testing Application E2E

--- src/main/resources/database/data.sql
+++ src/main/resources/database/data.sql
@@ -4,9 +4,9 @@ INSERT INTO OFFER VALUES ('3', 'Trabajador de limpieza', '2022-06-01', '2022-08-
 
 INSERT INTO APPLICATIONS VALUES ('1', '1', '1'); 
 INSERT INTO APPLICATIONS VALUES ('2', '1', '2');
-INSERT INTO APPLICATIONS VALUES ('4', '3', '2');
-INSERT INTO APPLICATIONS VALUES ('5', '3', '1');
-INSERT INTO APPLICATIONS VALUES ('6', '2', '3');
+INSERT INTO APPLICATIONS VALUES ('3', '3', '2');
+INSERT INTO APPLICATIONS VALUES ('4', '3', '1');
+INSERT INTO APPLICATIONS VALUES ('5', '2', '3');
 
 INSERT INTO USER VALUES ('1', 'Pedro', 'Cuevas', 'Teleco', 'pedro@telefonica.com', '{noop}12345', 'ROLE_ADMIN');
 INSERT INTO USER VALUES ('2', 'Isabela', 'Goetsch', 'Business', 'isabela@gmail.com', '{bcrypt}67891', 'ROLE_USER');
