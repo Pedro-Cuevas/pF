0717ef134b41157c8aabc7dfb0aa4c7c3c5ac6e9
Pedro-Cuevas <prcuevasolarte@gmail.com>
2022-05-16 20:10:47 +0200

    que asco de asignatura es a veces

--- src/main/java/com/telefonica/pF/config/SecurityConfig.java
+++ src/main/java/com/telefonica/pF/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -19,6 +20,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserService userService;
@@ -45,9 +47,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()
             .authorizeRequests()
-                .antMatchers("/admin.html").hasRole("ADMIN")
                 .antMatchers("/", "/index.html", "/news.html", "/bolsa.html", "/assets/js/**", "/assets/img/**", "/assets/css/**", "/assets/vendor/**", "/api/v1/**", "/assets/login/**").permitAll()
                 .anyRequest().authenticated()
             .and()
@@ -60,7 +60,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutSuccessUrl("/index.html")
                 .invalidateHttpSession(true)
                 .deleteCookies("JSESSIONID")
-            );
+            )
+            .csrf().disable();
     }
 
     @Override
