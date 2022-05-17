55372dcf0e3ee0b9da35921360b6dabc55846c02
Pedro-Cuevas <prcuevasolarte@gmail.com>
2022-05-17 10:29:05 +0200

    quitando basura

--- src/main/java/com/telefonica/pF/model/UserModel.java
+++ src/main/java/com/telefonica/pF/model/UserModel.java
@@ -1,14 +1,9 @@
 package com.telefonica.pF.model;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import lombok.Builder;
 import lombok.Data;
